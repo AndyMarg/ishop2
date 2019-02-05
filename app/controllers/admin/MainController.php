d42a0c6547bfc24b0fc35688a9b3c1bc7330b315
Andrey Margashov <Andrey.Margashov@yandex.ru>
2019-02-05 12:10:35 +0300

    Admin common info panels finished (Lesson 3 finished)

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -3,11 +3,18 @@
 namespace app\controllers\admin;
 
 
+use core\base\Application;
+
 class MainController extends AdminController
 {
 
     public function indexAction()
     {
+        $cnt_new_order = (int)(Application::getDb()->query("select count(*) cnt from `order` where status = '0'")[0]['cnt']);
+        $cnt_user = (int)(Application::getDb()->query("select count(*) cnt from `user`")[0]['cnt']);
+        $cnt_product = (int)(Application::getDb()->query("select count(*) cnt from `product`")[0]['cnt']);
+        $cnt_category = (int)(Application::getDb()->query("select count(*) cnt from `category`")[0]['cnt']);
         $this->getView()->setMeta("Панель администрирования");
+        $this->getView()->setData(compact('cnt_new_order', 'cnt_user', 'cnt_product', 'cnt_category'));
     }
 }
--- app/views/admin/Main/index.php
+++ app/views/admin/Main/index.php
@@ -1,66 +1,63 @@
+<?php
+    $admin_path = \core\base\Application::getConfig()->dirs->admin;
+    $www_path = \core\base\Application::getConfig()->dirs->www;
+?>
+
 <section class="content-header">
     <h1>Панель управления </h1>
     <ol class="breadcrumb">
-        <li><a href="<?=\core\base\Application::getConfig()->dirs->admin?>"><i class="fa fa-home"></i>Главная</a></li>
+        <li><a href="<?=$admin_path?>"><i class="fa fa-home"></i>Главная</a></li>
     </ol>
 </section>
 
 <section class="content">
     <div class="row">
         <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
             <div class="small-box bg-aqua">
                 <div class="inner">
-                    <h3>150</h3>
-
-                    <p>New Orders</p>
+                    <h3><?=$cnt_new_order?></h3>
+                    <p>Новых заказов</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=$admin_path?>/order" class="small-box-footer">Все заказы <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
             <div class="small-box bg-green">
                 <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                    <p>Bounce Rate</p>
+                    <h3><?=$cnt_product?></h3>
+                    <p>Товаров</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=$admin_path?>/product" class="small-box-footer">Все товары <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
             <div class="small-box bg-yellow">
                 <div class="inner">
-                    <h3>44</h3>
-
-                    <p>User Registrations</p>
+                    <h3><?=$cnt_user?></h3>
+                    <p>Пользователей</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=$admin_path?>/user" class="small-box-footer">Список пользователей <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
             <div class="small-box bg-red">
                 <div class="inner">
-                    <h3>65</h3>
-
-                    <p>Unique Visitors</p>
+                    <h3><?=$cnt_category?></h3>
+                    <p>Категорий товаров</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=$admin_path?>/category" class="small-box-footer">Список категорий <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
     </div>
